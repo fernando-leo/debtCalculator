70a4eb213413a6e499d1411a8b0732ea1184ee3a
fernando.ruffino <fernando.ruffino@scanbrazilconsulting.com>
2020-10-30 18:16:47 -0300

    criação da api

---
+++ DebtCalc/InterestType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtCalc
+{
+	public class InterestType
+	{
+		public string Name { get; set; }
+		public int Value { get; set; }
+
+		public InterestType(string v1, int v2)
+		{
+			Name = v1;
+			Value = v2;
+		}
+	}
+}
